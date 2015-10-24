36ffa59a2674a53d56744a739fa589cf4b4d7734
Abraham Arce <xe1gyq@gmail.com>
2015-10-24 17:05:05 +0000

    Core: xLcdRgb Class
    Allow user to set RGB color on LCD with english name
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ xlcdrgb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import matplotlib.colors as colors
+import pyupm_i2clcd as lcd
+from colour import Color
+
+class xLcdRgb(object):
+
+    def __init__(self):
+
+        self.lcdrgb = lcd.Jhd1313m1(1, 0x3E, 0x62)
+
+    def __del__(self):
+        pass
+
+    def hexToRgb(self, value):
+        value = value.lstrip('#')
+        lv = len(value)
+        return list(tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3)))
+
+    def clear(self):
+        self.lcdrgb.clear()
+
+    def setCursor(self, x, y):
+        self.lcdrgb.setCursor(x,y)
+
+    def setColor(self, color):
+        color = Color(color)
+        colordecimal = self.hexToRgb(colors.rgb2hex(color.rgb))
+        self.lcdrgb.setColor(colordecimal[0], colordecimal[1], colordecimal[2])
+
+    def setText(self, text):
+        self.lcdrgb.write(text)
+
+# End of Text
