e916f04d9db99140f091cb768a83d699ba806c23
Abraham Arce <xe1gyq@gmail.com>
2015-11-02 04:02:39 +0000

    Core: xFaceRecognition Base Code
    Base code for xFaceRecognition
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ xfacerecognition.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+import cv2
+
+class xFaceRecognition(object):
+
+    def __init__(self):
+        self.imageinput = "output/in.jpeg"
+        self.cascPath = "configuration/haarcascade_frontalface_alt.xml"
+        self.imageoutput = "output/out.jpeg"
+
+    def detect(self):
+        faceCascade = cv2.CascadeClassifier(self.cascPath)
+        image = cv2.imread(self.imageinput)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        print "Found {0} faces!".format(len(faces))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imwrite(self.imageoutput, image)
+        cv2.waitKey(0)
+
+if __name__ == "__main__":
+
+    xfaces = xFaceRecognition()
+    xfaces.detect()
