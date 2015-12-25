0fa3e05a302c1ad265bd8aa330e49a30e445ea39
Abraham Arce <abraham.arce.moreno@intel.com>
2015-12-25 00:08:24 -0600

    Core: xVoice Clean Up
    Clean up for xvoice module
    
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- xvoice.py
+++ xvoice.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 
 import commands
+import os
 import subprocess
 import time
 
 class xVoice(object):
 
-    def __init__(self):
+    def __init__(self, voicefile="voice.wav"):
+        self.directorycurrent = os.path.dirname(os.path.realpath(__file__))
+        self.directoryoutput = self.directorycurrent + '/output/'
+        self.voicefile = self.directoryoutput + voicefile
         self.filename = "output/xvoice.wav"
         self.proc = None
 
-    def filenameset(self, filename):
-        self.filename = filename
+    def filenameset(self, voicefile):
+        self.voicefile = voicefile
 
-    def filenameget(self, filename):
-        return self.filename
+    def filenameget(self, voicefile):
+        return self.voicefile
 
     def recordstart(self):
-        args = ['arecord', '-D', 'plughw:1,0', '-t', 'wav', '-f', 'S16_LE', '-r', '48000', self.filename]
+        args = ['arecord', '-D', 'plughw:1,0', '-t', 'wav', '-f', 'S16_LE', '-r', '48000', self.voicefile]
         #args = ['arecord', '-t', 'wav', '-f', 'S16_LE', '-r', '48000', self.filename]
         proc = subprocess.Popen(args)
         print "PID:", proc.pid
@@ -33,11 +37,11 @@ class xVoice(object):
         self.recordstop(proc)
 
     def play(self):
-        status, output = commands.getstatusoutput("aplay " + self.filename)
+        status, output = commands.getstatusoutput("aplay " + self.voicefile)
         print output
 
     def erase(self):
-        status, output = commands.getstatusoutput("rm " + self.filename)
+        status, output = commands.getstatusoutput("rm " + self.voicefile)
 
 if __name__ == "__main__":
 
