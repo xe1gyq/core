52c85e2343d47935eb6f28f1b2a998fd3ff3aaf0
Abraham Arce <xe1gyq@gmail.com>
2015-12-24 20:57:38 -0500

    Core: xMqttPub Publish
    Publish method changed
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- xmqttpub.py
+++ xmqttpub.py
@@ -4,18 +4,20 @@ import paho.mqtt.client as paho
 
 class xMqttPub(object):
 
-    def __init__(self):
+    def __init__(self, server="test.mosquitto.org", port=1883):
+        self.server = server
+        self.port = port
         self.client = paho.Client()
         self.client.on_publish = self.on_publish
-        self.client.connect("test.mosquitto.org", 1883, 60)
+        self.client.connect(self.server, self.port, 60)
 
     def on_publish(self, mosq, obj, msg):
-        print("Value set!")
+        print("xMqttPub Value Published!")
 
-    def write(self, value):
-        self.client.publish("temp/random", value)
+    def write(self, topic, value):
+        self.client.publish(topic, value)
 
 if __name__ == "__main__":
 
-    xmqttpub = xMqttPub()
-    xmqttpub.write("43")
+    xmqttpub = xMqttPub(server="test.mosquitto.org", port=1883)
+    xmqttpub.write("temp/random", "43")
