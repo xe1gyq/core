ea37f5ba460c1c3b62a9f519d85176fadc6632e4
Abraham Arce <xe1gyq@gmail.com>
2016-05-16 02:47:04 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,4 @@
 
 # apt-get install python-pip python-opencv curl mpg123 python-pygame libxft-dev
 opkg install python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
-pip install paho-mqtt wolframalpha
-easy_install matplotlib
+pip install paho-mqtt wolframalpha matplotlib
