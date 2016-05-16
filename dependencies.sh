dc4145d46d4a77b1a77393e9591e63bf0055c84d
Abraham Arce <xe1gyq@gmail.com>
2016-05-16 02:46:37 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # apt-get install python-pip python-opencv curl mpg123 python-pygame libxft-dev
-opkg install python-dev fswebcam alsa-utils mpg123 espeak libxft-dev
+opkg install python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
 pip install paho-mqtt wolframalpha
 easy_install matplotlib
