4ddfe19a0b45833aacf19d68fa540d70c866e669
Abraham Arce <xe1gyq@gmail.com>
2016-05-22 06:34:28 +0000

    Twython Dependencies
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- dependencies.sh
+++ dependencies.sh
@@ -2,4 +2,4 @@
 
 # apt-get install python-pip python-opencv curl mpg123 python-pygame libxft-dev
 opkg install python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
-pip install paho-mqtt wolframalpha matplotlib
+pip install paho-mqtt wolframalpha matplotlib twython --target /home/root/
