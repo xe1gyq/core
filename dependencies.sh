5742b545c7c1ec44af519eb20d2d402ced866dab
Abraham Arce <xe1gyq@gmail.com>
2016-05-28 03:46:31 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get install python-pip python-dev python-opencv curl mpg123 python-pygame libxft-dev
-opkg install python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
+opkg install python-opencv python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
 pip install wit SpeechRecogntion paho-mqtt wolframalpha matplotlib twython --target /home/root/
