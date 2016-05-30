46c5231aa30c409e860cf9021e0fa1d5d3f9f178
Abraham Arce <xe1gyq@gmail.com>
2016-05-29 19:12:17 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 apt-get install python-pip python-dev python-opencv curl mpg123 python-pygame libxft-dev
-opkg install python-opencv python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev
+opkg install python-opencv python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev 
+opkg install libjack
+opkg install --nodeps jack-dev 
+opkg install libportaudio-dev
 pip install wit SpeechRecogntion paho-mqtt wolframalpha matplotlib twython --target /home/root/
