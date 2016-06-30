e56e60c70d28ed0329922caea5f6d4ab076313db
Abraham Arce <xe1gyq@gmail.com>
2016-06-30 07:26:25 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get install python-pip python-dev python-opencv curl mpg123 python-pygame libxft-dev
+# apt-get install python-pip python-dev python-opencv curl mpg123 python-pygame libxft-dev
 opkg install python-opencv python-dev fswebcam alsa-utils mpg123 espeak libxft-dev libpng-dev 
 opkg install libjack
 opkg install --nodeps jack-dev 
